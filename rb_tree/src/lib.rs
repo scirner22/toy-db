55736085524bc45e3b9dc753f789bf4e76968ee3
Stephen Cirner <scirner@figure.com>
2022-10-06 21:34:00 -0400

    Implement contains and insert method.

--- rb_tree/src/lib.rs
+++ rb_tree/src/lib.rs
@@ -6,7 +6,11 @@ struct BinaryTree<T> {
     right: Option<Box<BinaryTree<T>>>,
 }
 
-impl<T> BinaryTree<T> {
+impl<T> BinaryTree<T>
+where
+    T: PartialEq,
+    T: PartialOrd,
+{
     fn new(data: T) -> Self {
         BinaryTree {
             data,
@@ -25,16 +29,53 @@ impl<T> BinaryTree<T> {
         self
     }
 
-    pub fn insert(&self, _data: T) -> Self {
-        todo!("implement")
+    pub fn insert(&mut self, data: T) {
+        let mut node = self;
+
+        loop {
+            if node.data == data {
+                return;
+            } else if node.data < data {
+                if let Some(ref mut right) = node.right {
+                    node = right;
+                } else {
+                    node.right = Some(Box::new(BinaryTree::new(data)));
+                    return;
+                }
+            } else {
+                if let Some(ref mut left) = node.left {
+                    node = left;
+                } else {
+                    node.left = Some(Box::new(BinaryTree::new(data)));
+                    return;
+                }
+            }
+        }
     }
 
-    pub fn remove(&self, _data: T) -> Self {
-        todo!("implement")
-    }
+    // pub fn remove(&mut self, data: T) -> Option<&BinaryTree<T>> {
+    // }
 
-    pub fn contains(&self, _data: T) -> bool {
-        todo!("implement")
+    pub fn contains(&mut self, data: T) -> bool {
+        let mut node = self;
+
+        loop {
+            if node.data == data {
+                return true;
+            } else if node.data < data {
+                if let Some(ref mut right) = node.right {
+                    node = right;
+                } else {
+                    return false;
+                }
+            } else {
+                if let Some(ref mut left) = node.left {
+                    node = left;
+                } else {
+                    return false;
+                }
+            }
+        }
     }
 }
 
@@ -68,5 +109,42 @@ mod tests {
         assert!(right_tree.left.is_none());
         assert_eq!(right_tree.data, 20);
         assert_eq!(right_tree.right.unwrap().data, 25);
+    }
+
+    #[test]
+    fn contains() {
+        let mut tree = BinaryTree::new(10)
+            .left(
+                BinaryTree::new(5)
+                    .left(BinaryTree::new(2))
+                    .right(BinaryTree::new(7)),
+            )
+            .right(BinaryTree::new(20).right(BinaryTree::new(25)));
+
+        assert!(tree.contains(2));
+        assert!(tree.contains(5));
+        assert!(tree.contains(7));
+        assert!(tree.contains(10));
+        assert!(tree.contains(20));
+        assert!(tree.contains(25));
+
+        assert!(!tree.contains(100));
+    }
+
+    #[test]
+    fn insert_and_remove() {
+        let mut tree = BinaryTree::new(1);
+
+        tree.insert(5);
+        tree.insert(0);
+        tree.insert(6);
+        tree.insert(10);
+        tree.insert(7);
+
+        assert!(tree.contains(0));
+        assert!(tree.contains(5));
+        assert!(tree.contains(6));
+        assert!(tree.contains(10));
+        assert!(tree.contains(7));
     }
 }
